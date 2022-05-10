9afac7de97bb8fc2df458a936a5c9f4b13f39234
Steve Larsen <slarsen-3@att.net>
2022-05-09 21:53:06 -0500

    added login & logout functions

---
+++ main.js
@@ -0,0 +1,29 @@
+// console.log('Hello World');
+
+// connect to Moralis server
+const serverUrl = 'https://pd42rdl6s4we.usemoralis.com:2053/server';
+const appId = 'WOFAkhXlUxeDp6WCpoD7i3QRGXC161cQqDwvz3CO';
+Moralis.start({ serverUrl, appId });
+
+login = async () => {
+  await Moralis.Web3.authenticate().then(async function (user) {
+    console.log('logged in');
+    user.set('name', document.getElementById('user-username').value);
+    user.set('email', document.getElementById('user-email').value);
+    await user.save();
+    // console.log(Moralis.User.current());
+    window.location.href = 'dashboard.html';
+  });
+};
+
+logout = async () => {
+  await Moralis.User.logOut();
+  window.location.href = 'index.html';
+};
+
+if (document.querySelector('#btn-login') != null) {
+  document.querySelector('#btn-login').onclick = login;
+}
+if (document.querySelector('#btn-logout') != null) {
+  document.querySelector('#btn-logout').onclick = logout;
+}
